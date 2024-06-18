9699d93888336ce283608775f802e4a2bdc77a0d
ZorkaA <68531996+ZorkaA@users.noreply.github.com>
2024-06-18 21:03:36 +0300

    Update index.js

--- .github/actions/update-stats/index.js
+++ .github/actions/update-stats/index.js
@@ -1,20 +1,39 @@
 const { google } = require('googleapis');
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
 
-async function fetchData() {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: './service_account_key.json',
+const statsFile = path.join(__dirname, '../../../stats.json');
+const googleDriveApiKey = process.env.GOOGLE_DRIVE_API_KEY;
+const googleDriveFolderId = process.env.GOOGLE_DRIVE_FOLDER_ID;
+
+async function main() {
+  const auth = new google.auth.JWT({
+    email: process.env.GOOGLE_DRIVE_SERVICE_ACCOUNT_EMAIL,
+    key: process.env.GOOGLE_DRIVE_API_KEY.replace(/\\n/g, '\n'),
     scopes: ['https://www.googleapis.com/auth/drive.readonly'],
   });
 
   const drive = google.drive({ version: 'v3', auth });
 
-  const folderId = process.env.GOOGLE_DRIVE_FOLDER_ID;
-  const response = await drive.files.list({
-    q: `'${folderId}' in parents and mimeType='text/csv'`,
-    fields: 'files(id, name, createdTime)',
+  const res = await drive.files.list({
+    q: `'${googleDriveFolderId}' in parents and mimeType='text/csv' and name contains 'apidb'`,
+    fields: 'files(id, name)',
   });
 
-  return response.data.files;
+  const files = res.data.files;
+  const dates = files.map(file => {
+    const datePart = file.name.split('_')[0];
+    return new Date(datePart);
+  });
+
+  const stats = {
+    firstPull: dates.length ? new Date(Math.min(...dates)).toDateString() : 'N/A',
+    lastPull: dates.length ? new Date(Math.max(...dates)).toDateString() : 'N/A',
+    totalPulls: files.length,
+  };
+
+  fs.writeFileSync(statsFile, JSON.stringify(stats, null, 2));
 }
 
-fetchData().then(files => console.log(files)).catch(err => console.error(err));
+main().catch(console.error);
